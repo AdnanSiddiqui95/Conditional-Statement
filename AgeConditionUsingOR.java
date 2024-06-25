be9448cc4a2e614ed709931b11dc8100236fb4c6
Adnan Siddiqui <sid.adnan9569@gmail.com>
2024-06-25 17:17:07 +0530

    Add files via upload

---
+++ AgeConditionUsingOR.java
@@ -0,0 +1,15 @@
+public class AgeConditionUsingOR
+{
+	public static void main(String[] agrs)
+	  {
+		int age=27;
+		if(age<20 || age>25)
+		{
+		    System.out.println("Invalid Candidate");
+		}
+		else
+		{
+		    System.out.println("Valid Candidate");
+		}
+	  }
+}
