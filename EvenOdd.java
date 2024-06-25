748e2d2a4f9cd466b27e63a0c15eee2d9627a68c
Adnan Siddiqui <sid.adnan9569@gmail.com>
2024-06-25 17:00:17 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,15 @@
+public class EvenOdd
+{
+	public static void main(String[] agrs)
+	  {
+		int num=25;
+		if(num%2==0)
+		{
+		    System.out.println("Number is even");
+		}
+		else
+		{
+		    System.out.println("Number is odd");
+		}
+	  }
+}
