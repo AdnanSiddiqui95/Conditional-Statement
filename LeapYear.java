97e3b991f281457c428d98cbca413619da669592
Adnan Siddiqui <sid.adnan9569@gmail.com>
2024-06-25 17:28:48 +0530

    Add files via upload

---
+++ LeapYear.java
@@ -0,0 +1,15 @@
+public class LeapYear
+{
+	public static void main(String[] agrs)
+	  {
+		int year=2024;
+		if(year%4==0  &&  year%100!=0  || year%400==0)
+		{
+		    System.out.println("It is a Leap Year");
+		}
+		else
+		{
+		    System.out.println("It is not a Leap Year");
+		}
+	  }
+}
